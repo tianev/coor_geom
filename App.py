d213ba459b0c1d1cb3c2d554315830293b84f394
Evelina Tian <49968666+tianev@users.noreply.github.com>
2022-06-14 15:38:53 +1000

    Add database file

--- App.py
+++ App.py
@@ -1,77 +1,99 @@
 from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import sqlite3
+import os
 
+# конфигурация
+DATABASE = '/tmp/app.db'
+DEBUG = True
+SECRET_KEY = 'ksjvjdbvre4698g78ge7fee'
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] ='ksjvjdbvre4698g78ge7fee'
+app.config.from_object(__name__)
 
-menu = [{"name": "Главная", "url": "home"},
-        {"name": "Теория", "url": "theory"},
-        {"name": "Задачи", "url": "task"},
-        {"name": "Обратная связь", "url": "contact"},
-        {"name": "О сайте", "url": "about"}
-]
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'app.db')))
 
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
 
-@app.route('/')
-@app.route('/home')
-def index():
-    return render_template("index.html", menu=menu)
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
-
-@app.route('/about')
-def about():
-    print(url_for('about'))
-    return render_template("about.html", menu=menu)
-
-@app.route('/theory')
-def theory():
-    return render_template("theory.html", menu=menu)
-
-@app.route('/task')
-def task():
-    return render_template("task.html", menu=menu)
-
-@app.route("/contact", methods=["POST", "GET"])
-def contact():
-    # print(request.form)
-    if request.method == 'POST':
-        if len(request.form['username']) > 2:
-            flash('Сообщение отправлено', category='success')
-        else:
-            flash('Ошибка отправки', category='warning')
-
-    return render_template('contact.html', title="Обратная связь", menu=menu)
-
-
-@app.route("/profile/username")
-def profile(username):
-    if 'userLogged' not in session or session['userLogged'] != username:
-        abort(404)
-
-    return f"Профиль пользователя: {username}"
-
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    print(request.form)
-    print(session)
-    if 'userLogged' in session:
-        return redirect(url_for('profile', username=session['userLogged']))
-    elif request.method == 'POST' and request.form['username'] == "coor@geom.com" and request.form['psw'] == "123":
-        session['userLogged'] = request.form['username']
-        return redirect(url_for('profile', username=session['userLogged']))
-
-    return render_template('login.html', title='Авторизация', menu=menu)
-
-
-
-@app.errorhandler(404)
-def pageNotFount(error):
-    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
-
-# with app.test_request_context():
-#     print(url_for('index'))
-
-
-if __name__=="__main__":
-    app.run(debug=True)
+# app.config['SECRET_KEY'] ='ksjvjdbvre4698g78ge7fee'
+#
+# menu = [{"name": "Главная", "url": "home"},
+#         {"name": "Теория", "url": "theory"},
+#         {"name": "Задачи", "url": "task"},
+#         {"name": "Обратная связь", "url": "contact"},
+#         {"name": "О сайте", "url": "about"}
+# ]
+#
+#
+# @app.route('/')
+# @app.route('/home')
+# def index():
+#     return render_template("index.html", menu=menu)
+#
+#
+# @app.route('/about')
+# def about():
+#     print(url_for('about'))
+#     return render_template("about.html", menu=menu)
+#
+# @app.route('/theory')
+# def theory():
+#     return render_template("theory.html", menu=menu)
+#
+# @app.route('/task')
+# def task():
+#     return render_template("task.html", menu=menu)
+#
+# @app.route("/contact", methods=["POST", "GET"])
+# def contact():
+#     # print(request.form)
+#     if request.method == 'POST':
+#         if len(request.form['username']) > 2:
+#             flash('Сообщение отправлено', category='success')
+#         else:
+#             flash('Ошибка отправки', category='warning')
+#
+#     return render_template('contact.html', title="Обратная связь", menu=menu)
+#
+#
+# @app.route("/profile/username")
+# def profile(username):
+#     if 'userLogged' not in session or session['userLogged'] != username:
+#         abort(404)
+#
+#     return f"Профиль пользователя: {username}"
+#
+#
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     print(request.form)
+#     print(session)
+#     if 'userLogged' in session:
+#         return redirect(url_for('profile', username=session['userLogged']))
+#     elif request.method == 'POST' and request.form['username'] == "coor@geom.com" and request.form['psw'] == "123":
+#         session['userLogged'] = request.form['username']
+#         return redirect(url_for('profile', username=session['userLogged']))
+#
+#     return render_template('login.html', title='Авторизация', menu=menu)
+#
+#
+#
+# @app.errorhandler(404)
+# def pageNotFount(error):
+#     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+#
+# # with app.test_request_context():
+# #     print(url_for('index'))
+#
+#
+# if __name__=="__main__":
+#     app.run(debug=True)

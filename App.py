0c340c76777c5694cfef1c06ec9c36699d3a586a
Evelina Tian <49968666+tianev@users.noreply.github.com>
2022-06-14 15:11:05 +1000

    Add authorization

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 
 app = Flask(__name__)
@@ -33,13 +33,41 @@ def task():
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
+    # print(request.form)
     if request.method == 'POST':
         if len(request.form['username']) > 2:
-            flash('Сообщение отправлено')
+            flash('Сообщение отправлено', category='success')
         else:
-            flash('Ошибка отправки')
+            flash('Ошибка отправки', category='warning')
 
-    return render_template('contact.html', menu=menu)
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+
+@app.route("/profile/username")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(404)
+
+    return f"Профиль пользователя: {username}"
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    print(request.form)
+    print(session)
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "coor@geom.com" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 # with app.test_request_context():
 #     print(url_for('index'))
